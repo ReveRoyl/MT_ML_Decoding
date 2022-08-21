f4b3d5966bac8571adce474a77f610c707121ce2
ReveRoyl <roylvpn@gmail.com>
2022-08-21 19:38:11 +0100

    21/08

--- CNN/Code/code/models.py
+++ CNN/Code/code/models.py
@@ -1,3 +1,17 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+from torch.cuda import amp
+import torch.nn.functional as F
+import math
+from scipy.integrate import simps
+from band_power import (
+    bandpower_multi_bands,
+    standard_scaling_sklearn,
+)
+import sklearn
+
 class RPS_MNet(nn.Module):
     """
         Model inspired by [Aoe at al., 10.1038/s41598-019-41500-x] integrated with bandpower.
@@ -119,7 +133,7 @@ class RPS_MNet(nn.Module):
         return x.squeeze(1)
     
 class LSTM1(nn.Module):
-    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+    def __init__(self, num_classes=14, input_size=272, hidden_size=10, num_layers=1, seq_length=272):
         super(LSTM1, self).__init__()
         self.num_classes = num_classes #number of classes
         self.num_layers = num_layers #number of layers
@@ -134,9 +148,9 @@ class LSTM1(nn.Module):
 
         self.relu = nn.ReLU()
     
-    def forward(self,x):
-        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device) #hidden state
-        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device) #internal state
+    def forward(self,x,pb):
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda() #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda()#internal state
         # Propagate input through LSTM
         output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
         hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
@@ -184,3 +198,120 @@ class CNN(nn.Module):
       # return output, x
       # x=self.softmax(x)
       return x
+
+
+
+class ASRCNet(nn.Module):
+    """
+        Model inspired by [Aoe at al., 10.1038/s41598-019-41500-x] integrated with bandpower.
+    """
+    class Showsize(nn.Module):
+        def __init__(self):
+            super(ASRCNet.Showsize, self).__init__()
+        def forward(self, x):
+            # print(x.shape)
+            return x
+
+    def __init__(self, n_times):
+        """
+        Args:
+            n_times (int):
+                n_times dimension of the input data.
+        """
+        super(ASRCNet, self).__init__()
+        # if n_times == 501:  # TODO automatic n_times
+        #     self.n_times = 12
+        # elif n_times == 601:
+        #     self.n_times = 18
+        # elif n_times == 701:
+        #     self.n_times = 24
+        # else:
+        #     raise ValueError(
+        #         "Network can work only with n_times = 501, 601, 701 "
+        #         "(epoch duration of 1., 1.2, 1.4 sec),"
+        #         " got instead {}".format(n_times)
+        #     )
+        self.n_times = n_times
+        self.spatial = nn.Sequential(
+            nn.Conv2d(1, 32, stride=(1, 1), kernel_size=(272,64), bias=False), #kernel size 204, 64
+            nn.ReLU(),
+            # nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, stride=(1, 1), kernel_size=(1, 16), bias=False), # kernel size 1,16
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2)),
+            # nn.BatchNorm2d(64),
+        )
+
+        self.temporal = nn.Sequential(
+            nn.Conv2d(1, 32, stride=(1, 1), kernel_size=(8, 8), bias=False),
+            nn.ReLU(),
+            # nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, stride=(1, 1), kernel_size=(8, 8), bias=False),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(5, 3), stride=(5, 3)),
+            # nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, stride=(1, 1), kernel_size=(1, 4), bias=False),
+            nn.ReLU(),
+            # nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, stride=(1, 1), kernel_size=(1, 4), bias=False), #conv6
+            nn.ReLU(),
+            # nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2)),
+            nn.Conv2d(64, 128, stride=(1, 1), kernel_size=(1, 2), bias=False),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            # nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, stride=(1, 1), kernel_size=(1, 2), bias=False),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            # nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2)),
+            nn.Conv2d(128, 256, stride=(1, 1), kernel_size=(1, 2), bias=False),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            # nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, stride=(1, 1), kernel_size=(1, 2), bias=False), #conv10
+            nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            # nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2)),
+        )
+
+        # self.attention = nn.Sequential(
+        #     ChannelAttention([None, 256, 64, self.n_times]),
+        #     SpatialAttention(),
+        # )
+
+        self.concatenate = Concatenate()
+
+        self.flatten = Flatten_MEG()
+
+        self.ff1 = nn.Sequential(
+            # nn.Linear(256 * 26 * self.n_times + 272 * 6, 1024),
+            nn.Linear(34912, 1024),
+            # nn.BatchNorm1d(num_features=1024),
+            nn.ReLU(),
+            self.Showsize(),
+            nn.Linear(1024, 1024),
+            # nn.BatchNorm1d(num_features=1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            self.Showsize(),
+        )
+        self.ff2 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(1024 , 14),
+        )
+        self.softmax = nn.Softmax(dim =1)
+
+    def forward(self, x, pb):
+        x = self.spatial(x)
+        x = torch.transpose(x, 1, 2)
+        x = self.temporal(x)
+        # x = self.attention(x)
+        x = self.concatenate(x, pb)
+        x = self.ff1(self.flatten(x))
+        x = self.ff2(x)
+        x = self.softmax(x)
+
+        return x.squeeze(1)
